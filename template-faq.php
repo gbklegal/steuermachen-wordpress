3b1d63729ad60d2cf8a314acc909d24197ee746f
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-01-18 17:29:29 +0100

    Add Tabs

--- template-faq.php
+++ template-faq.php
@@ -22,11 +22,53 @@ get_header();
         <?php the_content(); ?>
         </section>
 
-        <section>
+        <section class="section-mt easy-with-tax-section">
             <header class="text-center">
-                <h2>So einfach geht's mit Steuern</h2>
+                <h2 class="mb-0">So einfach geht's mit Steuern</h2>
                 <h3 class="font-normal">Angst vor der Steuererklärung? - Keine Panik</h3>
             </header>
+
+            <main>
+
+                <div class="tabs-wrapper">
+
+                    <div class="changeable-part" data-tabs="content">
+                        <div class="content content-selected">
+                            <img src="https://picsum.photos/id/198/800/600" alt="">
+                        </div>
+                        <div class="content">
+                            <img src="https://picsum.photos/id/221/800/600" alt="">
+                        </div>
+                        <div class="content">
+                            <img src="https://picsum.photos/id/238/800/600" alt="">
+                        </div>
+                        <div class="content">
+                            <img src="https://picsum.photos/id/1037/800/600" alt="">
+                        </div>
+                    </div>
+
+                    <div class="selections" data-tabs="selector">
+                        <div class="item item-selected">
+                            <h3>Beantragen Sie die gewünschte Steuererklärung</h3>
+                            <p>Füllen Sie das Bestellformular aus, wählen Sie das gewünschte aus Steuerjahr, für das Sie die Einkommensteuererklärung gemacht und einfach abschicken die Form.</p>
+                        </div>
+                        <div class="item">
+                            <h3>Dokumente vorbereiten und versenden</h3>
+                            <p>Füllen Sie das Bestellformular aus, wählen Sie das gewünschte aus Steuerjahr, für das Sie die Einkommensteuererklärung gemacht und einfach abschicken die Form.</p>
+                        </div>
+                        <div class="item">
+                            <h3>Ihr persönliches Finanzamt bietet die Lösung für Ihre Steuererklärung</h3>
+                            <p>Fill out the order form, select the desired tax year for which you want to have the income tax return made and simply submit the form.</p>
+                        </div>
+                        <div class="item">
+                            <h3>Fertig: Sie erhalten Ihre geprüfter Steuerbescheid</h3>
+                            <p>Wenn Ihr Steuerbescheid vorliegt vom Finanzamt erstellt, wird es von Ihren Steuerexperten geprüft und an Sie weitergeleitet.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </main>
         </section>
 
         <section class="curvy-video-section">
