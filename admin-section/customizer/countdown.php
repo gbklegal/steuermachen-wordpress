079ed8ee8eeac90e0932552c76e1f7b6a3d5427f
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-09-07 16:06:39 +0200

    Update countdown.php
    Prettier

--- admin-section/customizer/countdown.php
+++ admin-section/customizer/countdown.php
@@ -2,35 +2,25 @@
 
 /**
  * Countdown
- * 
+ *
  * Adds the individual sections, settings, and controls to the theme customizer
  */
-$wp_customize->add_section(
-    'countdown_section',
-    array(
-        'title' => 'Countdown',
-        'priority' => 35
-    )
-);
+$wp_customize->add_section('countdown_section', [
+    'title' => 'Countdown',
+    'priority' => 40,
+]);
 
+$wp_customize->add_setting('countdown_end');
 
-$wp_customize->add_setting(
-    'countdown_end'
-);
-
-$wp_customize->add_control(
-    'countdown_end',
-    array(
-        'label' => 'Countdown Ende',
-        'description' => 'Dieses Datum wird als Standardwert für den Countdown Shortcode hergenommen.',
-        'section' => 'countdown_section',
-        'type' => 'datetime-local',
-        'input_attrs' => array(
-            'min' => substr( date('c'), 0, 16 )
-        )
-    )
-);
-
+$wp_customize->add_control('countdown_end', [
+    'label' => 'Countdown Ende',
+    'description' => 'Dieses Datum wird als Standardwert für den Countdown Shortcode hergenommen.',
+    'section' => 'countdown_section',
+    'type' => 'datetime-local',
+    'input_attrs' => [
+        'min' => substr(date('c'), 0, 16),
+    ],
+]);
 
 // TODO uncomment this section when it can be implemented
 // $wp_customize->add_setting(
