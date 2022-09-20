28afc9f22ed6764b0130a238f956bd2d349661c9
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-09-20 15:02:48 +0200

    fa-landing becomes publisher

---
+++ footer-publisher.php
@@ -0,0 +1,45 @@
+    <footer class="max-width">
+        <div class="footer-inner">
+            <?php the_custom_logo(); ?>
+            <p class="text-justify">Copyright &copy; steuermachen (GDF). Sämtliche Inhalte, Fotos, Texte und Graphiken sind urheberrechtlich geschützt. Sie dürfen ohne vorherige schriftliche Zustimmung oder Genehmigung der GDF weder ganz noch auszugsweise kopiert, verändert, vervielfältigt oder veröffentlicht werden.</p>
+            <ul>
+                <li>
+                    <a href="javascript:modalFrame.show(113)" class="text-primary">Impressum</a>
+                </li>
+                <li>&bull;</li>
+                <li>
+                    <a href="javascript:modalFrame.show(110)" class="text-primary">Datenschutz</a>
+                </li>
+                <li>&bull;</li>
+                <li>
+                    <a href="javascript:modalFrame.show(123)" class="text-primary">Demo</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+
+
+    <div id="modalWrapper">
+        <div id="modalClose" class="btn btn-primary">schließen <i class="icon-x"></i></div>
+        <div class="loader u-spin icon-loader"></div>
+        <div id="modalFrame" class="prose"></div>
+    </div>
+
+    <div class="back-to-top-wrapper">
+        <a id="back-to-top" href="#">
+            <i class="icon-chevrons-up"></i>
+        </a>
+    </div>
+
+</div>
+</div>
+
+<script>
+var faCookieExp = 90;
+</script>
+<script src="https://fat.financeads.net/fpc.js"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- header-publisher.php
+++ header-publisher.php
@@ -1,25 +1,26 @@
 <?php
 /**
  * The header for the steuermachen theme.
- * 
+ *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
- * 
+ *
  * @see https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
 
 /**
  * financeAds
  */
-// TODO: uncomment in prod.
-// $fa_cookie_name = 'NetworkInfo';
-// $fa_cookie_value = $_GET['s_id'];
-// $fa_cookie_duration = time() + (86400 * 90); // = 90 Tage Laufzeit;
-// $fa_cookie_path = '/';
-// $fa_cookie_domain= 'https://steuermachen.de/'; //bspw. "https://steuermachen.de/"
-// $fa_cookie_secure = true; //= dieser Cookie darf nur per HTTPS ausgelesen werden
-// $fa_cookie_httponly = true; //= Cookie darf nicht von Javascript-Dateien ausgelesen werden
-// $fa_cookie_samesite = 'SameSite=Lax';
- 
+if (isset($_GET['s_id'])) {
+    $fa_cookie_name = 's_id';
+    $fa_cookie_value = $_GET['s_id'];
+    $fa_cookie_duration = time() + 86400 * 90; // = 90 Tage Laufzeit;
+    $fa_cookie_path = '/';
+    $fa_cookie_domain = 'https://steuermachen.de/'; //bspw. "https://steuermachen.de/"
+    $fa_cookie_secure = true; //= dieser Cookie darf nur per HTTPS ausgelesen werden
+    $fa_cookie_httponly = true; //= Cookie darf nicht von Javascript-Dateien ausgelesen werden
+    $fa_cookie_samesite = 'SameSite=Lax';
+}
+
 // setcookie($fa_cookie_name, $fa_cookie_value, $fa_cookie_duration, $fa_cookie_path, $fa_cookie_domain, $fa_cookie_secure, $fa_cookie_httponly, $fa_cookie_samesite);
 
 wp_enqueue_style('steuermachen-style', get_bloginfo('stylesheet_url'));
@@ -28,15 +29,15 @@ wp_enqueue_script_footer('steuermachen-fa-landing-script', get_template_director
 
 $_chistmas = '';
 
-if ( true === is_christmas_time() )
+if (true === is_christmas_time()) {
     $_chistmas = 'christmas';
-
+}
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title><?php echo wp_get_document_title(); ?></title>
@@ -47,12 +48,12 @@ if ( true === is_christmas_time() )
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
 
 </head>
-<body <?php body_class( $_chistmas ); ?>>
+<body <?php body_class($_chistmas); ?>>
 
     <div id="page">
 
     <div class="container">
 
         <section class="section-stm-logo mb-0 py-2 max-width">
-            <img src="<?php echo wp_get_attachment_url( get_theme_mod( 'custom_logo' ) ); ?>" class="custom-logo" alt="steuermachen">
+            <img src="<?php echo wp_get_attachment_url(get_theme_mod('custom_logo')); ?>" class="custom-logo" alt="steuermachen">
         </section>
