8d740443f17ad088a708b4cd1708f533efa51352
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-09-20 15:06:51 +0200

    Update fa-landing.js

--- js/fa-landing.js
+++ js/fa-landing.js
@@ -1,10 +1,10 @@
 const modalFrame = new ModalFrame({
-    modalElmt: document.querySelector('#modal'),
+    modalElmt: document.querySelector('#modalWrapper'),
     closeElmt: document.querySelector('#modalClose'),
-    frameElmt: document.querySelector('#modalFrame')
+    frameElmt: document.querySelector('#modalFrame'),
 });
 
-const footerLinks = document.querySelectorAll('footer a');
-footerLinks.forEach(link => {
-    link.addEventListener('click', modal.open);
-});
+// const footerLinks = document.querySelectorAll('footer a');
+// footerLinks.forEach(link => {
+//     link.addEventListener('click', modal.open);
+// });>
