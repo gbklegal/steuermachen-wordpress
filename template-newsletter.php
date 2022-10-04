5f54c427c018cecb5b606189b472891c37714e3d
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-10-04 10:20:04 +0200

    Update template-newsletter.php

--- template-newsletter.php
+++ template-newsletter.php
@@ -4,18 +4,21 @@
  * Template Name: Newsletter
  */
 
-add_filter( 'body_class', function( $classes ) {
-    return array_merge( $classes, [ 'no-gradient' ] );
-} );
+add_filter('body_class', function ($classes) {
+    return array_merge($classes, ['no-gradient', 'bg-transparent']);
+});
 
 $_subscribe_newsletter = isset($_GET['subscribe_newsletter']) ?? false;
+$_is_frame_mode = is_frame_mode();
+$_frame_mode = $_is_frame_mode ? '' : ' class="main-content"';
 
 get_header();
-
 ?>
-
-<main>
-    <?php if ( false === $_subscribe_newsletter ): ?>
+<main<?= $_frame_mode ?>>
+    <?php if (!$_is_frame_mode): ?>
+    <h1><?= the_title() ?></h1>
+    <?php endif; ?>
+    <?php if (false === $_subscribe_newsletter): ?>
     <form action="<?php echo manipulate_get_params(['subscribe_newsletter' => '']); ?>" method="post">
         <div class="row">
             <div class="field-wrapper">
@@ -36,10 +39,10 @@ get_header();
             </div>
         </div>
         <div class="row">
-            <input class="btn btn-primary btn-full" type="submit" value="Anmelden">
+            <input class="btn btn-full" type="submit" value="Anmelden">
         </div>
     </form>
-    <p class="text-grey text-xs mt-3 text-justify mb-0">Für den Versand unserer Newsletter nutzen wir rapidmail. Mit deiner Anmeldung stimmst du zu, dass die eingegebenen Daten an rapidmail übermittelt werden. Beachte bitte deren <a href="https://www.rapidmail.de/agb" target="_blank" rel="noreferrer noopener">AGB</a> und <a href="https://www.rapidmail.de/datenschutz-kundenbereich" target="_blank" rel="noreferrer noopener">Datenschutzbestimmungen</a>.</p>
+    <p class="mt-3 mb-0 text-xs text-justify text-grey">Für den Versand unserer Newsletter nutzen wir rapidmail. Mit deiner Anmeldung stimmst du zu, dass die eingegebenen Daten an rapidmail übermittelt werden. Beachte bitte deren <a href="https://www.rapidmail.de/agb" target="_blank" rel="noreferrer noopener">AGB</a> und <a href="https://www.rapidmail.de/datenschutz-kundenbereich" target="_blank" rel="noreferrer noopener">Datenschutzbestimmungen</a>.</p>
     <?php else: ?>
     <div class="newsletter-final">
         <?php echo get_image_tag(32994, 'lächelnder Panda', '', 'center', [100, 100]); ?>
@@ -49,6 +52,4 @@ get_header();
     <?php endif; ?>
 </main>
 
-<?php
-
-get_footer();
+<?php get_footer();
