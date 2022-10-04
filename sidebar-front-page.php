7532d3f95c7ea0219b45e95cac9f118ba3e2f324
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-10-04 10:17:39 +0200

    Update Sidebar
    remove the ability to select blog posts from customizer

--- sidebar-front-page.php
+++ sidebar-front-page.php
@@ -1,27 +1,24 @@
 <div class="sidebar sidebar-front-page">
+    <?php if (false): ?>
     <div class="sidebar-item newsletter">
-        <h3 class="text-center sidebar-item-title br-2">Erhalte <span class="text-primary">kostenlose</span> Steuerberatungen und Updates!</h3>
+        <h3 class="p-4 text-center">Erhalte <span class="text-primary">kostenlose</span> Steuerberatungen und Updates!</h3>
         <div class="sidebar-item-content">
             <?php echo get_image_tag(32993, 'steuermachen App herunterladen', 'steuermachen App herunterladen', 'center', [100, 100]); ?>
             <!-- <iframe class="newsletter-iframe" src="https://tf53be01c.emailsys1a.net/139/6219/913c50802d/subscribe/form.html?_g=1651052315" frameborder="0" width="290" height="654"></iframe> -->
             <iframe class="iframe-loading" src="/newsletter/?frame_mode&hide_admin_bar" frameborder="0" scrolling="no" onload="adjustIframe(this)" class="newsletter-frame"></iframe>
         </div>
     </div>
-    <div class="sidebar-item why-us">
-        <div class="text-center text-white sidebar-item-title bg-secondary br-2">Warum steuermachen?</div>
-        <div class="sidebar-item-content">Wir sind keine Steuersoftware, kein Chatbot. Deine Steuererklärung wird von einem echten Steuerexperten schnell und sicher bearbeitet. Dein persönlicher Steuerexperte beantwortet deine Fragen und hilft dir, deine maximale Rückerstattung zu bekommen!</div>
-    </div>
-    <?php if (false): ?>
+    <?php endif; ?>
     <div class="text-center sidebar-item new-product">
-        <h3 class="text-white sidebar-item-title bg-primary br-2">Unser neustes Produkt!</h3>
+        <h3 class="p-4">Grundsteuererklärung</h3>
         <div class="sidebar-item-content">
-            <h4>grundsteuererklärungMACHEN</h4>
-            <a href="/steuererklaerung-beauftragen/?product=grundsteuererklärungMACHEN" class="btn btn-secondary">Jetzt beauftragen</a>
+            <img class="br-4" src="<?= wp_get_attachment_image_url(34146, 'medium') ?>" alt="Grundsteuererklärung">
+            <p>Grundstückseigentümer müssen jetzt eine Grundsteuererklärung machen lassen. Lass jetzt auch deine Grundsteuererklärung anfertigen!</p>
+            <a href="/steuererklaerung-beauftragen/?product=grundsteuererklärungMACHEN" class="btn">Jetzt Steuern sparen</a>
         </div>
     </div>
-    <?php endif; ?>
     <div class="sidebar-item qr-code-app-download">
-        <h3 class="pb-0 text-center sidebar-item-title">QR scannen & App herunterladen</h3>
+        <h3 class="p-4 text-center">QR scannen & App herunterladen</h3>
         <div class="sidebar-item-content">
             <a href="/app">
                 <?php echo get_image_tag(32987, 'steuermachen App herunterladen', 'steuermachen App herunterladen', 'left', [200, 200]); ?>
@@ -29,30 +26,26 @@
         </div>
     </div>
     <div class="sidebar-item contact-form">
-        <h3 class="text-center sidebar-item-title">Kontaktformular</h3>
+        <h3 class="p-4 text-center">Kontaktformular</h3>
         <div class="sidebar-item-content"><?php echo do_shortcode('[contact-form-7 id="30" title="Kontaktformular"]'); ?></div>
     </div>
     <div class="sidebar-item steuerratgeber">
-        <h3 class="text-center text-white sidebar-item-title bg-secondary br-2">Steuerratgeber</h3>
+        <h3 class="p-4 text-center">Steuerratgeber</h3>
         <!-- <div class="sidebar-content"><a href="/steuerratgeber/">Steuerratgeber</a></div> -->
         <div class="sidebar-item-content">
             <div class="latest-posts">
             <?php
-            $quick_link_ids = [get_theme_mod('quick_link_1'), get_theme_mod('quick_link_2'), get_theme_mod('quick_link_3'), get_theme_mod('quick_link_4')];
-            $quick_link_ids = array_filter($quick_link_ids, function ($item) {
-                if (false === empty($item) && false !== $item) {
-                    return $item;
-                }
-            });
-            $quick_link_ids_count = count($quick_link_ids);
+            $recent_posts_options = [
+                'numberposts' => 4,
+                'post_status' => 'publish',
+            ];
+            $recent_posts = wp_get_recent_posts($recent_posts_options);
 
-            foreach ($quick_link_ids as $key => $quick_link_id):
+            foreach ($recent_posts as $key => $post):
 
-                $post = get_post($quick_link_id);
-
-                $post_id = $post->ID;
+                $post_id = $post['ID'];
                 $post_url = get_permalink($post_id);
-                $post_title = $post->post_title;
+                $post_title = $post['post_title'];
                 $post_thumbnail = get_the_post_thumbnail($post_id, 'medium');
                 ?>
                 <div class="post">
@@ -69,7 +62,7 @@
                         </a>
                     </div>
                 </div>
-                <?php if ($key + 1 < $quick_link_ids_count): ?>
+                <?php if ($key + 1 < 4): ?>
                 <hr class="my-3">
                 <?php endif; ?>
             <?php
