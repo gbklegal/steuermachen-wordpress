04192925c34ed72b289ee92823e4d4ba564d7705
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-10-04 10:17:52 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,21 +14,35 @@
 
         <hr>
 
-        <div class="text-center sign-up">
+        <div class="text-center bg-white sign-up br-2 box-shadow-small">
             <!-- <h2 class="uppercase">Jetzt registrieren! Keine Verpflichtung kostenlose Testverion (ist dieser Text wirklich final?)</h2>
             <p>Melde Dich bei Think-Tax an und mache deine persönliche Tour durch Think-Tax (ist auch dieser Text wirklich final?)</p>
-            <a href="/dashboard" class="btn btn-secondary btn-lg">Anmelden</a> -->
+            <a href="/dashboard" class="btn btn-primary btn-lg">Anmelden</a> -->
             <iframe src="https://tf53be01c.emailsys1a.net/139/6219/913c50802d/subscribe/form.html?_g=1651052315" frameborder="0" width="100%" height="345" id="newsletter"></iframe>
         </div>
 
-        <div class="text-center app-badges">
-            <a href="/app/ios">
-                <?php echo stm_get_image_tag(32577, 'App Store', 'steuermachen - App Store', 'left', [200]); ?>
-            </a>
-            <a href="/app/android">
-                <?php echo stm_get_image_tag(32578, 'Play Store', 'steuermachen - Play Store', 'left', [200]); ?>
-            </a>
+        <div id="sicherheit" class="text-center bg-white br-2 box-shadow-small">
+            <div class="section-inner">
+                <h2>Deine Sicherheit hat höchste Priorität, jetzt und immer.</h2>
+                <ul class="benefits">
+                    <li><i class="icon-shield-check"></i> Verschlüsselung</li>
+                    <li><i class="icon-database"></i> Datenspeicherung</li>
+                    <li><i class="icon-lock"></i> Datenschutz</li>
+                </ul>
+                <!-- <div class="more-details">
+                    <a href="#">Erfahre mehr über Sicherheit</a>
+                </div> -->
+            </div>
+            <div class="text-center app-badges">
+                <a href="/app/ios">
+                    <?php echo stm_get_image_tag(32577, 'App Store', 'steuermachen - App Store', 'left', [200]); ?>
+                </a>
+                <a href="/app/android">
+                    <?php echo stm_get_image_tag(32578, 'Play Store', 'steuermachen - Play Store', 'left', [200]); ?>
+                </a>
+            </div>
         </div>
+
 
         <div class="footer-navs">
             <div class="footer-nav">
@@ -36,7 +50,7 @@
                 <?php get_nav_menu('footer_1'); ?>
             </div>
             <div class="footer-nav">
-                <h3>Rechtliche Dokumente</h3>
+                <h3>Dokumente</h3>
                 <?php get_nav_menu('footer_2'); ?>
             </div>
             <div class="footer-nav">
@@ -49,7 +63,7 @@
         </div>
         <div class="footer-text" id="copyright">
             <p>Copyright &copy; steuermachen (GDF). Sämtliche Inhalte, Fotos, Texte und Graphiken sind urheberrechtlich geschützt. Sie dürfen ohne vorherige schriftliche Zustimmung oder Genehmigung der GDF weder ganz noch auszugsweise kopiert, verändert, vervielfältigt oder veröffentlicht werden.</p>
-            <p>&ldquo;TaxGreen – Papierlos glücklich&rdquo; ist die Kampagne von steuermachen.de, die den Plan und die Maßnahmen von steuermachen.de zum Schutz dieser Erde und Umwelt umfasst.</p>
+            <p>&ldquo;TaxGreen – Papierlos glücklich&rdquo; ist die Kampagne von steuermachen, die den Plan und die Maßnahmen von steuermachen.de zum Schutz dieser Erde und Umwelt umfasst.</p>
         </div>
         <p><small>* Preise inkl. 19% Mwst. *** Preise netto zzgl. der gesetzlichen Mehrwertsteuer von 19%.<br>
         ** Wichtiger Hinweis zum Bruttojahreseinkommen: Die Bemessungsgrundlage setzt sich zusammen aus: - dem auf der Lohnsteuerkarte des betreffenden Steuerjahres eingetragenen Bruttoarbeitslohn - dem jährlichen Gesamtbetrag der Renten, Versorgungsbezüge, Lohnersatzleistungen und Unterhaltsleistungen - den Einnahmen aus Vermietung und Verpachtung, sowie den Einnahmen aus Kapitalvermögen. Im Falle von zusammenveranlagten Ehegatten werden die zuvor genannten Bemessungsgrundlagen und Einnahmen zusammengerechnet.</small></p>
