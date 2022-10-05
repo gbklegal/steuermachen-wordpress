499fec50ddb1dba5c1a01369fe7fe6deba91cdde
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-10-05 16:00:44 +0200

    Update Nav

--- header.php
+++ header.php
@@ -7,28 +7,12 @@
  * @see https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
 
-// TODO remove in prod.
-$_contenteditable = isset($_GET['edit']) ? ' contenteditable="true"' : '';
-
-// add_enqueue_script_attributes(, 'defer');
-// wp_enqueue_style('steuermachen-style-old', 'https://steuermachen.de/wp-content/themes/onepress-child/style.css');
 wp_enqueue_style('steuermachen-style', get_bloginfo('stylesheet_url'));
-// wp_enqueue_script_footer('steuermachen-cookie-script', get_template_directory_uri() . '/js/js.cookie.min.js');
 wp_enqueue_script_footer('steuermachen-script', get_template_directory_uri() . '/js/script.js');
 ?>
 <!DOCTYPE html>
-<html <?php
-language_attributes();
-echo $_contenteditable;
-?>>
+<html <?php language_attributes(); ?>>
 <head>
-
-    <!-- CSS only -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-    <!-- JavaScript Bundle with Popper -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-
-    <!-- <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"> -->
 
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -51,9 +35,6 @@ echo $_contenteditable;
                         <a href="<?php echo home_url(); ?>">
                             <?php if (has_custom_logo()): ?>
                                 <?php
-                                # the_custom_logo();
-                                ?>
-                                <?php
                                 $custom_logo_srcset = '';
                                 if (has_custom_retina_logo()) {
                                     $custom_logo_srcset = wp_get_attachment_url(get_custom_retina_logo()) . ' 2x';
@@ -66,22 +47,20 @@ echo $_contenteditable;
                             <?php get_nav_menu('primary'); ?>
                         </div>
                     </div>
-                    <div class="header-part">
-                        <div class="hidden-on-mobile">
-                            <a href="/beauftragen" class="btn btn-primary">Jetzt Beauftragen</a>
-                        </div>
+                    <div class="header-part cta-wrapper">
+                        <a href="/beauftragen" class="btn btn-primary">Jetzt beauftragen</a>
                     </div>
                 </div>
                 <div class="header-mobile">
                     <i class="icon-menu" data-show-menu></i>
+                    <i class="icon-x" style="display: none;" data-hide-menu></i>
 
                     <div class="header-mobile-overlay" id="menu-mobile">
-                        <i class="icon-x" data-hide-menu></i>
-
                         <div class="header-mobile-overlay-inner">
                             <?php get_nav_menu('primary'); ?>
-                            <!-- TODO: convert button to a link button -->
-                            <a href="/beauftragen" class="btn btn-primary">Jetzt Beauftragen</a>
+                            <div class="cta-wrapper">
+                                <a href="/beauftragen" class="btn btn-primary">Jetzt beauftragen</a>
+                            </div>
                         </div>
                     </div>
                 </div>
