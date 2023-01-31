e1fec380fd677ae6da58800553b67b04f82b4558
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2023-01-31 12:16:18 +0100

    Update search.php

--- search.php
+++ search.php
@@ -1,31 +1,30 @@
-<?php get_header(); ?>
+<?php
+wp_enqueue_script('inview-script', get_stylesheet_directory_uri() . '/js/inview.js');
+wp_enqueue_script('livesearch-script', get_stylesheet_directory_uri() . '/js/livesearch.js');
+
+get_header();
+?>
 <main class="main-content">
     <?php
-        $the_query = new WP_Query(['s' => get_search_query()]);
-        $search_results_count = $the_query->found_posts;
-        $search_result_text = 'Suchergebnis';
+    $the_query = new WP_Query(['s' => get_search_query()]);
+    $search_results_count = $the_query->found_posts;
+    $search_result_text = 'Suchergebnis';
 
-        if ($search_results_count < 1 || $search_results_count > 1)
-            $search_result_text .= 'se';
-
-        if ($the_query->have_posts()):
-            _e("<h2>{$search_results_count} {$search_result_text} für: <strong>" . get_query_var('s') . "</strong></h2>");
-        ?>
-        <div class="search-results">
-        <?php 
-            while ($the_query->have_posts()):
-                $the_query->the_post();
-            ?>
+    if ($search_results_count !== 1) {
+        $search_result_text .= 'se';
+    }
+    ?>
+    <h2><?= $search_results_count ?> <?= $search_result_text ?> für: <strong><?= get_query_var('s') ?></strong></h2>
+    <div class="search-results">
+    <?php if ($the_query->have_posts()): ?>
+        <?php while ($the_query->have_posts()):
+            $the_query->the_post(); ?>
                 <div class="search-result">
                     <a class="h3" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                     <?php the_excerpt(); ?>
                 </div>
             <?php
-            endwhile;
-        ?>
-        </div>
-        <?php
-        endif;
-    ?>
+        endwhile;endif; ?>
+    </div>
 </main>
 <?php get_footer();
