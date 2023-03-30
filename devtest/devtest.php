be862d667a8a6ee7198c4752a085d71f7c31fd03
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2023-03-30 11:35:06 +0200

    Update devtest.php

--- devtest/devtest.php
+++ devtest/devtest.php
@@ -4,9 +4,16 @@
  * Template Name: DEV Test
  */
 
-$_ml = function () {
-    var_dump('Hello World');
-};
-$_ml();
+// $_ml = function () {
+//     var_dump('Hello World');
+// };
+// $_ml();
 
 // sprintf('');
+
+wp_head();
+
+// var_dump(wp_title('|', false));
+var_dump(get_query_var('m'));
+
+wp_footer();
